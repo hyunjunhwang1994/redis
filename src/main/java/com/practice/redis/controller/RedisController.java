73d859f526ca7e8c5a26a51fdbbb276ca821ca18
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-03-17 19:25:16 +0900

    Feat: 레디스 파일 추가
    레디스 컨트롤러 및 서비스 클래스 추가

---
+++ src/main/java/com/practice/redis/controller/RedisController.java
@@ -0,0 +1,12 @@
+package com.practice.redis.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedisController {
+
+
+
+}
---
+++ src/main/java/com/practice/redis/service/RedisService.java
@@ -0,0 +1,4 @@
+package com.practice.redis.service;
+
+public class RedisService {
+}
