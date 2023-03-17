4754025994f9c375b36c830b64c1fe64ceb5c951
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-03-17 19:07:50 +0900

    feat : 레디스 설정 파일 추가
    레디스 사용 전 설정파일 추가 및 설정

---
+++ src/main/java/com/practice/redis/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.practice.redis.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private String redisPort;
+
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean(name = "defaultRedisConnectionFactory")
+    public RedisConnectionFactory tokenRedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(Integer.parseInt(redisPort));
+        redisStandaloneConfiguration.setPassword(redisPassword);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        return lettuceConnectionFactory;
+    }
+
+}
