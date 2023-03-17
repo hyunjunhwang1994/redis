57754e8ad6d4d3e94a6a837de64dc3bd05bfb63c
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-03-17 19:49:41 +0900

    feat: Add 레디스 컨트롤러 템플릿과 레포지토리 방식 냉무

--- src/main/java/com/practice/redis/controller/RedisController.java
+++ src/main/java/com/practice/redis/controller/RedisController.java
@@ -2,11 +2,23 @@ package com.practice.redis.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/redis")
 public class RedisController {
 
+    @GetMapping("/template")
+    public void redisTemplate() {
+
+    }
+
+    @GetMapping("/repository")
+    public void redisRepository() {
+
+    }
 
 
 }
